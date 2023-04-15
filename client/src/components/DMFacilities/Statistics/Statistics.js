bcee7086a52cc32b005b66000f8c5b64a3827911
Bartek <cierniabartek0802@gmail.com>
2023-04-16 00:38:43 +0200

    Added statistics file

---
+++ client/src/components/DMFacilities/Statistics/Statistics.js
@@ -0,0 +1,10 @@
+const Statistics = () => {
+    return(
+        <div>
+            Statistics
+        {/*TODO charts and statistics about session time(totalTime, fightingTime, roleplayTime, break...
+           TODO Statistics about players crits, killed monsters, saved characters
+        */}
+        </div>
+    )
+}
