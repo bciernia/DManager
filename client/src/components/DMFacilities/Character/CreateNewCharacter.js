31e9b37274868631c0b3b87848bf028e3ce290c1
Bartek <cierniabartek0802@gmail.com>
2023-04-17 21:33:53 +0200

    Created choosing character type view

--- client/src/components/DMFacilities/Character/CreateNewCharacter.js
+++ client/src/components/DMFacilities/Character/CreateNewCharacter.js
@@ -1,60 +1,93 @@
-import {Formik} from "formik";
-import {CharacterInitialValues} from "../../../utils/dndUtils/CharacterInitialValues";
-import {Box, Button, TextField} from "@mui/material";
-import * as yup from 'yup'
-import InputField from "../../../utils/InputField";
-import MultiStepForm, {FormStep} from "../../../utils/MultiStepForm";
-
-//TODO finish validation schemas
-const characterInfoValidationSchema = yup.object({
-    characterName: yup.string().required('Character name is required'),
-    playerName: yup.string().required('Player name is required'),
-    characterType: yup.string().required('Character type is required'),
-    characterClass: yup.string().required('Character class is required'),
-    characterRace: yup.string().required('Character race is required'),
-    characterBackground: yup.string().required('Character background is required'),
-    characterBackstory: yup.string().length(1000, "Backstory cannot be longer than 1000 characters"),
-});
-
-const characterStatsValidationSchema = yup.object({
-    characterStrength: yup.number().min(1, "Stat has to be higher than 1").max(30, "Stat has to be lower than 30").required("Strength is required"),
-    characterDexterity: yup.number().min(1, "Stat has to be higher than 1").max(30, "Stat has to be lower than 30").required("Dexterity is required"),
-    characterConstitution: yup.number().min(1, "Stat has to be higher than 1").max(30, "Stat has to be lower than 30").required("Constitution is required"),
-    characterIntelligence: yup.number().min(1, "Stat has to be higher than 1").max(30, "Stat has to be lower than 30").required("Intelligence is required"),
-    characterWisdom: yup.number().min(1, "Stat has to be higher than 1").max(30, "Stat has to be lower than 30").required("Wisdom is required"),
-    characterCharisma: yup.number().min(1, "Stat has to be higher than 1").max(30, "Stat has to be lower than 30").required("Charisma is required"),
-});
+import {Box, Button, Grid, TextField, Typography} from "@mui/material";
+import beastImg from '../../../assets/newCharacterTypePhotos/beast.jpg'
+import monsterImg from '../../../assets/newCharacterTypePhotos/monster.jpg'
+import playerCharacterImg from '../../../assets/newCharacterTypePhotos/playerCharacter.jpg'
+import npcImg from '../../../assets/newCharacterTypePhotos/npc.jpg'
+import classes from './CreateNewCharacter.module.css'
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 const CreateNewCharacter = () => {
+    const [chosenTypeLabel, setChosenTypeLabel] = useState('Character type'); //napis
+    const [chosenType, setChosenType] = useState('');// dla obiektu
+
+    const navigate = useNavigate()
+
+    const handleRadio = (characterTypeLabel, characterType) => {
+        setChosenTypeLabel(characterTypeLabel);
+        setChosenType(characterType);
+    }
+
+    const goToAddNewNpcView = () => {
+        if(chosenType === ''){
+            alert("You have to chose character type");
+            return;
+        }
+
+        navigate(`${chosenType}`);
+    }
+
     return (
-        <Box sx={{height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-            <MultiStepForm initialValues={CharacterInitialValues}
-                           onSubmit={values => {
-                               console.log(values, null, 2)
-                           }}
-            >
-                {/*TODO finish adding character form*/}
-                <FormStep
-                    stepName="Character info"
-                    onSubmit={() => console.log('Step1 submit')}
-                    validationSchema={characterInfoValidationSchema}>
-                    <InputField name="characterName" label="Character name" sx={{margin: ".25rem 0"}}/>
-                    <InputField name="playerName" label="Player name" sx={{margin: ".25rem 0"}}/>
-                </FormStep>
-                <FormStep
-                    stepName="Character skills"
-                    onSubmit={() => console.log('Step2 submit')}
-                    validationSchema={characterStatsValidationSchema}>
-                    <InputField name="characterStrength" label="Character strength"
-                                sx={{margin: ".25rem 0", minWidth: "10rem", maxWidth: "20rem"}}/>
-                    <InputField name="characterDexterity" label="Character dexterity" sx={{margin: ".25rem 0"}}/>
-                    <InputField name="characterDexterity" label="Character dexterity" sx={{margin: ".25rem 0"}}/>
-                    <InputField name="characterDexterity" label="Character dexterity" sx={{margin: ".25rem 0"}}/>
-                    <InputField name="characterDexterity" label="Character dexterity" sx={{margin: ".25rem 0"}}/>
-                    <InputField name="characterDexterity" label="Character dexterity" sx={{margin: ".25rem 0"}}/>
-                </FormStep>
-            </MultiStepForm>
-        </Box>
+        <div>
+            <Box sx={{width: "100%", height: "100%"}}>
+                <Grid container
+                      spacing={1}
+                >
+                    <Grid item md={12}>
+                        <Typography>Choose type of new character</Typography>
+                    </Grid>
+                    <Grid item md={12}>
+                        <Typography>{chosenTypeLabel}</Typography>
+                    </Grid>
+                    <Grid item md={12}>
+                        <Button onClick={goToAddNewNpcView}>
+                            Create
+                        </Button>
+                    </Grid>
+                    <Grid item md={6}>
+                        <label>
+                            <input type="radio" name="test"
+                                   className={classes.createNewCharacterRadioBtn}
+                                   onChange={() => handleRadio('Player character', 'playerCharacter')}
+                            />
+                            <img src={playerCharacterImg}/>
+                        </label>
+                    </Grid>
+
+                    <Grid item md={6}>
+                        <label>
+                            <input type="radio" name="test" value="small"
+                                   className={classes.createNewCharacterRadioBtn}
+                                   onChange={() => handleRadio('Monster', 'monster')}
+                            />
+                            <img src={monsterImg}/>
+                        </label>
+
+                    </Grid>
+
+                    <Grid item md={6}>
+                        <label>
+                            <input type="radio" name="test" value="small"
+                                   className={classes.createNewCharacterRadioBtn}
+                                   onChange={() => handleRadio('Beast', 'beast')}
+                            />
+                            <img src={beastImg}/>
+                        </label>
+                    </Grid>
+
+                    <Grid item md={6}>
+                        <label>
+                            <input type="radio" name="test" value="small"
+                                   className={classes.createNewCharacterRadioBtn}
+                                   onChange={() => handleRadio('Non-player character', 'npc')}
+                            />
+                            <img src={npcImg}/>
+                        </label>
+                    </Grid>
+                </Grid>
+
+            </Box>
+        </div>
     )
 }
 
