cb94741c3d810623e0d07df22d2620b7722eb561
Bartek <cierniabartek0802@gmail.com>
2023-04-19 21:22:04 +0200

    added character aligment helper

--- client/src/utils/dndUtils/CharacterAlignments.js
+++ client/src/utils/dndUtils/CharacterAlignments.js
@@ -1,3 +1,11 @@
 export const CharacterAlignments = {
-
+    LG: "Lawful Good",
+    NG: "Neutral Good",
+    CH: "Chaotic Good",
+    LN: "Lawful Neutral",
+    TN: "True Neutral",
+    CN: "Chaotic Neutral",
+    LE: "Lawful Evil",
+    NE: "Neutral Evil",
+    CE: "Chaotic Evil",
 }
