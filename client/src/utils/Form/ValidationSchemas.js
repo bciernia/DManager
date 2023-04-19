dcc4cb0e3d85ae1d070128141a4f020c4d100dfd
Bartek <cierniabartek0802@gmail.com>
2023-04-19 21:14:42 +0200

    added new validation to fields

--- client/src/utils/Form/ValidationSchemas.js
+++ client/src/utils/Form/ValidationSchemas.js
@@ -16,7 +16,7 @@ export const characterInfoValidationSchema = yup.object({
     characterClass: yup.string().required("Character class is required").oneOf(characterClassesArray),
     // characterRace: yup.string().required('Character race is required'),
     // characterBackground: yup.string().required('Character background is required'),
-    characterBackstory: yup.string().max(1000, "Backstory cannot be longer than 1000 characters"),
+    characterBackstory: yup.string().max(1000, "Backstory cannot be longer than 1000 characters"), //TODO on last page
     // characterAllies: yup.string().length(250,"Character allies cannot be longer than 250 characters"),
     // characterIdeals: yup.string().length(250,"Character ideals cannot be longer than 250 characters"),
     // characterPersonalityTraits: yup.string().length(250,"Character personality traits cannot be longer than 250 characters"),
@@ -28,10 +28,23 @@ export const characterInfoValidationSchema = yup.object({
 });
 
 export const characterStatsValidationSchema = yup.object({
+    characterLevel: yup.number().min(1, "Level cannot be lower than 1").max(20, "Level cannot be higher than 20").required("Level is required"),
     characterStrength: yup.number().min(1, "Stat has to be higher than 1").max(30, "Stat has to be lower than 30").required("Strength is required"),
     characterDexterity: yup.number().min(1, "Stat has to be higher than 1").max(30, "Stat has to be lower than 30").required("Dexterity is required"),
     characterConstitution: yup.number().min(1, "Stat has to be higher than 1").max(30, "Stat has to be lower than 30").required("Constitution is required"),
     characterIntelligence: yup.number().min(1, "Stat has to be higher than 1").max(30, "Stat has to be lower than 30").required("Intelligence is required"),
     characterWisdom: yup.number().min(1, "Stat has to be higher than 1").max(30, "Stat has to be lower than 30").required("Wisdom is required"),
     characterCharisma: yup.number().min(1, "Stat has to be higher than 1").max(30, "Stat has to be lower than 30").required("Charisma is required"),
+    exp: yup.number().min(0, "Character experience can not be lower than 0").max(400000, "Character experience has to be lower than 400000").required("Character experience is required"),
+    characterSpeed: yup.number().min(1, "Speed has to be higher than 1ft").max(500, "Speed has to be lower than 500ft").required("Speed is required"),
+    characterHP: yup.number().min(1, "Stat has to be higher than 1").max(1000, "Stat has to be lower than 30").required("Charisma is required"),
+    characterInitiative: yup.number().min(1, "Stat has to be higher than 1").max(30, "Stat has to be lower than 30").required("Charisma is required"),
+});
+
+export const characterProficienciesValidationSchema = yup.object({
+
+})
+
+export const characterSkillsValidationSchema = yup.object({
+
 });
