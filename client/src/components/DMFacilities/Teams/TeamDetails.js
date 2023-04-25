7fca829bb8aa81324eaae9a27ace9142497c4b00
Bartek <cierniabartek0802@gmail.com>
2023-04-25 23:03:34 +0200

    deleted unnecessary old button to adding character

--- client/src/components/DMFacilities/Teams/TeamDetails.js
+++ client/src/components/DMFacilities/Teams/TeamDetails.js
@@ -33,12 +33,12 @@ const TeamDetails = props => {
     return (
         <Wrapper>
             {isLoading && <Spinner/>}
-
+            <Button> <Link sx={{textDecoration: "none"}} to="../../" relative="path">Go back to summary</Link></Button>
             <Box sx={{display: 'flex', justifyContent: 'flex-start', flexWrap: 'wrap', flexDirection: 'row'}}>
                 {charactersArray.length === 0 && <p>No characters</p>}
-                {charactersArray.map(character => <Character key={character._id} teamId={teamId} character={character}/>)}
+                {charactersArray.map(character => <Character key={character._id} teamId={teamId}
+                                                             character={character}/>)}
             </Box>
-            <AddCharacter teamId={teamId} onUpdate={() => getAllCharacters(teamId)}/>
         </Wrapper>
     )
 }
