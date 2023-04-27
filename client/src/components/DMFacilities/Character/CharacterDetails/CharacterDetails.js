6698c15e0c1a8e0daf4e1ebeb4ce8b16ad18e5ae
Bartek <cierniabartek0802@gmail.com>
2023-04-27 22:35:47 +0200

    created statistics grid in character details

--- client/src/components/DMFacilities/Character/CharacterDetails/CharacterDetails.js
+++ client/src/components/DMFacilities/Character/CharacterDetails/CharacterDetails.js
@@ -27,10 +27,60 @@ const CharacterDetails = props => {
 
     return (
         <div>
-            <Box sx={{width: "75rem", height: "50rem", margin: "auto"}}>
+            <Box sx={{width: "75rem", height: "50rem", margin: "2rem auto"}}>
                 {isLoading && <Spinner/>}
                 <Grid container spacing={2}>
-                    <Grid item md={9} sx={{backgroundColor: "red", minHeight: "20rem"}}>
+                    <Grid item md={3} sx={{minHeight: "20rem"}}>
+                        <Typography variant="h4">{character.characterName}</Typography>
+                        <Typography variant="h4">{character.characterType}</Typography>
+                        <Typography variant="h4">{character.characterClass}</Typography>
+                        <Typography variant="h4">{character.characterRace || "No race"}</Typography>
+
+                    </Grid>
+                    <Grid item md={3} sx={{minHeight: "20rem"}}>
+                        <Grid container spacing={2} sx={{
+                            height: "100%",
+                            width: "100%",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center"
+                        }}>
+                            <Typography variant="h3" sx={{marginLeft: "1rem"}}>Statistics</Typography>
+                            <Grid item md={6} sx={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}><Typography display="inline">STR<Typography variant="h4" display="inline"><b> {character.characterStrength}</b></Typography></Typography></Grid>
+                            <Grid item md={6} sx={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}><Typography display="inline">DEX<Typography variant="h4" display="inline"><b> {character.characterDexterity}</b></Typography></Typography></Grid>
+                            <Grid item md={6} sx={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}><Typography display="inline">CON<Typography variant="h4"
+                                                                           display="inline"><b> {character.characterConstitution}</b></Typography></Typography></Grid>
+                            <Grid item md={6} sx={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}><Typography display="inline">INT<Typography variant="h4" display="inline"><b> {character.characterIntelligence}</b></Typography></Typography></Grid>
+                            <Grid item md={6} sx={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}><Typography display="inline">WIS<Typography variant="h4" display="inline"><b> {character.characterWisdom}</b></Typography></Typography></Grid>
+                            <Grid item md={6} sx={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}><Typography display="inline">CHA<Typography variant="h4"
+                                                                           display="inline"><b> {character.characterCharisma}</b></Typography></Typography></Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item md={3} sx={{minHeight: "20rem"}}>
                         <Typography variant="h4">{character.characterName}</Typography>
                         <Typography variant="h4">{character.characterType}</Typography>
                         <Typography variant="h4">{character.characterClass}</Typography>
