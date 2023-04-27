e9518940d8b22ecbf8e87efeab41b4f5a1d25afc
Bartek <cierniabartek0802@gmail.com>
2023-04-27 22:15:53 +0200

    added character race helper

--- client/src/utils/dndUtils/CharacterRaces.js
+++ client/src/utils/dndUtils/CharacterRaces.js
@@ -1,3 +1,11 @@
 export const CharacterRaces = {
-
+    dragonborn: "Dragonborn",
+    dwarf: "Dwarf",
+    elf: "Elf",
+    gnome: "Gnome",
+    halfElf: "Half-elf",
+    halfling: "Halfling",
+    halfOrc: "Half-orc",
+    human: "Human",
+    tiefling: "Tiefling",
 }
