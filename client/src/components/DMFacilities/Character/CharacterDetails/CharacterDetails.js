9dbdfecdb008a5b299059d59399976a01ac121e7
Bartek <cierniabartek0802@gmail.com>
2023-04-27 22:16:11 +0200

    Added character details module

--- client/src/components/DMFacilities/Character/CharacterDetails/CharacterDetails.js
+++ client/src/components/DMFacilities/Character/CharacterDetails/CharacterDetails.js
@@ -1,7 +1,8 @@
-import {Box, Button, Typography} from "@mui/material";
+import {Avatar, Box, Button, Grid, Typography} from "@mui/material";
 import {Link, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import Spinner from "../../../UI/Spinner/Spinner";
+import classes from "./CharacterDetails.module.css";
 
 const CharacterDetails = props => {
     const params = useParams();
@@ -20,17 +21,30 @@ const CharacterDetails = props => {
                 console.log(character);
                 setIsLoading(false);
             });
-    },[]);
+    }, []);
 
     //TODO finish character details page
 
     return (
         <div>
-            Character details
-    <Box>
-        {isLoading && <Spinner/>}
-        <Typography>{character.characterName}</Typography>
-    </Box>
+            <Box sx={{width: "75rem", height: "50rem", margin: "auto"}}>
+                {isLoading && <Spinner/>}
+                <Grid container spacing={2}>
+                    <Grid item md={9} sx={{backgroundColor: "red", minHeight: "20rem"}}>
+                        <Typography variant="h4">{character.characterName}</Typography>
+                        <Typography variant="h4">{character.characterType}</Typography>
+                        <Typography variant="h4">{character.characterClass}</Typography>
+                        <Typography variant="h4">{character.characterRace || "No race"}</Typography>
+
+                    </Grid>
+                    <Grid item md={3} sx={{minHeight: "20rem", position: "relative"}}>
+                        <Avatar src={character.characterPhoto}
+                                sx={{position: "relative", left: "2.5rem", width: '15rem', height: '15rem'}}/>
+                    </Grid>
+                    <Grid item md={12} sx={{backgroundColor: "blue", minHeight: "20rem"}}></Grid>
+                </Grid>
+
+            </Box>
             <Button> <Link sx={{textDecoration: "none"}} to="../../" relative="path">Go back to summary</Link></Button>
         </div>
     )
