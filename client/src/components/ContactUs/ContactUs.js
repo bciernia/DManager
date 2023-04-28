26dac658c6d4dae3c62ca45beccf8d9863ee38e0
Bartek <cierniabartek0802@gmail.com>
2023-04-29 00:20:28 +0200

    Created contact us form

--- client/src/components/ContactUs/ContactUs.js
+++ client/src/components/ContactUs/ContactUs.js
@@ -2,8 +2,15 @@ import {Divider, Grid} from "@mui/material";
 import classes from './ContactUs.module.css';
 import contactUsLogo from '../../assets/logo/contact_us_logo.png';
 import ContactUsForm from "./Form/ContactUsForm";
+import {useState} from "react";
 
 const ContactUs = () => {
+    const [userMessage, setUserMessage] = useState({});
+
+    const sendMail = (message) => {
+        setUserMessage(message);
+    }
+
     return (
         <div className={classes.container}>
             <Grid container sx={{height: "100%"}}>
@@ -18,7 +25,7 @@ const ContactUs = () => {
                 </Grid>
                 <Divider orientation="vertical" flexItem/>
                 <Grid item xs sx={{display: "flex", height: "100%", justifyContent: "center", alignItems: "center"}}>
-                    <ContactUsForm/>
+                    <ContactUsForm sendMail={sendMail}/>
                 </Grid>
             </Grid>
         </div>
--- client/src/components/ContactUs/Form/ContactUsForm.js
+++ client/src/components/ContactUs/Form/ContactUsForm.js
@@ -1,29 +1,51 @@
-import {FormControl, Input, InputLabel, TextField} from "@mui/material";
-import {Form, Formik} from "formik";
-import {FormStep} from "../../../utils/Form/MultiStepForm";
-import {mailValidationSchema} from "../../../utils/Form/ValidationSchemas";
+import {Button, Divider, TextField, Typography} from "@mui/material";
+import React, {useState} from "react";
+import classes from './ContactUsForm.module.css';
+import Card from "../../UI/Card/Card";
 
-const ContactUsForm = (onSubmit) => {
+const ContactUsForm = props => {
 
-    const sendMail = () => {
+    const [fullName, setFullName] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [emailSubject, setEmailSubject] = useState('');
+    const [emailContent, setEmailContent] = useState('');
 
-    }
+    const isNameValid = name => name.length < 2
 
-    const handleSubmit = async (values) => {
-        return onSubmit(values);
+    const handleSubmit = () => {
+
+
+        const userMessage = {
+            fullName,
+            emailAddress,
+            emailSubject,
+            emailContent,
+        }
+
+        props.sendMail(userMessage);
     }
 
     return (
-        <div>
-            <Formik onSubmit={handleSubmit} validationSchema={mailValidationSchema}>
-                {formik =>
-                    <Form onSubmit={formik.handleSubmit}>
-
-
-                    </Form>
-                }
-            </Formik>
-        </div>
+        <Card>
+            <Typography variant="h2" textAlign="center" marginBottom="1rem">Contact us!</Typography>
+            <form className={classes.form} onSubmit={handleSubmit}>
+                <TextField sx={{width: "80%"}} type="text" label="Full name"
+                           inputProps={{maxLength: 30}}
+                           onChange={(event) => setFullName(event.target.value)}/>
+                <TextField sx={{width: "80%"}} type="email" label="E-mail address"
+                           inputProps={{maxLength: 50}}
+                           onChange={(event) => setEmailAddress(event.target.value)}/>
+                <TextField sx={{width: "80%"}} type="text" label="E-mail subject"
+                           inputProps={{maxLength: 50}}
+                           onChange={(event) => setEmailSubject(event.target.value)}/>
+                <TextField sx={{width: "80%"}} type="text" inputProps={{maxLength: 1000}} multiline minRows={5}
+                           maxRows={5} label="E-mail content"
+                           onChange={(event) => setEmailContent(event.target.value)}/>
+                <div className={classes["characters-counter"]}><p>{emailContent.length}/1000</p></div>
+                <Button sx={{backgroundColor: "#F5793B" }} variant="contained" color="inherit"
+                        type="submit">Send</Button>
+            </form>
+        </Card>
     )
 }
 
