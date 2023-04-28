78f98a9e90022d8bf4427f10f37c28076c5f9573
Bartek <cierniabartek0802@gmail.com>
2023-04-29 00:30:58 +0200

    Added form validation

--- client/src/components/ContactUs/Form/ContactUsForm.js
+++ client/src/components/ContactUs/Form/ContactUsForm.js
@@ -2,6 +2,7 @@ import {Button, Divider, TextField, Typography} from "@mui/material";
 import React, {useState} from "react";
 import classes from './ContactUsForm.module.css';
 import Card from "../../UI/Card/Card";
+import {useField} from "formik";
 
 const ContactUsForm = props => {
 
@@ -9,11 +10,12 @@ const ContactUsForm = props => {
     const [emailAddress, setEmailAddress] = useState('');
     const [emailSubject, setEmailSubject] = useState('');
     const [emailContent, setEmailContent] = useState('');
+    const [touched, setTouched] = useState(false);
 
-    const isNameValid = name => name.length < 2
+    const isNameValid = name => name.length < 2 || name.length > 50;
+    const isMailSubjectValid = mail => mail.length > 50;
 
     const handleSubmit = () => {
-
 
         const userMessage = {
             fullName,
@@ -21,7 +23,6 @@ const ContactUsForm = props => {
             emailSubject,
             emailContent,
         }
-
         props.sendMail(userMessage);
     }
 
@@ -30,19 +31,24 @@ const ContactUsForm = props => {
             <Typography variant="h2" textAlign="center" marginBottom="1rem">Contact us!</Typography>
             <form className={classes.form} onSubmit={handleSubmit}>
                 <TextField sx={{width: "80%"}} type="text" label="Full name"
+                           error={touched && isNameValid(fullName)}
+                           helperText={(touched && isNameValid(fullName)) ? "Name must have more than 2 characters and less than 20" : ""}
                            inputProps={{maxLength: 30}}
+                           onBlur={() => setTouched(true)}
                            onChange={(event) => setFullName(event.target.value)}/>
                 <TextField sx={{width: "80%"}} type="email" label="E-mail address"
                            inputProps={{maxLength: 50}}
                            onChange={(event) => setEmailAddress(event.target.value)}/>
                 <TextField sx={{width: "80%"}} type="text" label="E-mail subject"
+                           error={isMailSubjectValid(emailSubject)}
+                           helperText={isMailSubjectValid(emailSubject) ? "E-mail subject can not be longer than 50 characters" : ""}
                            inputProps={{maxLength: 50}}
                            onChange={(event) => setEmailSubject(event.target.value)}/>
                 <TextField sx={{width: "80%"}} type="text" inputProps={{maxLength: 1000}} multiline minRows={5}
                            maxRows={5} label="E-mail content"
                            onChange={(event) => setEmailContent(event.target.value)}/>
                 <div className={classes["characters-counter"]}><p>{emailContent.length}/1000</p></div>
-                <Button sx={{backgroundColor: "#F5793B" }} variant="contained" color="inherit"
+                <Button sx={{backgroundColor: "#F5793B"}} variant="contained" color="inherit"
                         type="submit">Send</Button>
             </form>
         </Card>
