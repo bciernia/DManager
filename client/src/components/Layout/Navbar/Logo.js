8e034eb4550c82f640ebeb67bae1b5d9e4b088c0
Bartek <cierniabartek0802@gmail.com>
2023-04-28 21:35:21 +0200

    created logo as navlink to home page

--- client/src/components/Layout/Navbar/Logo.js
+++ client/src/components/Layout/Navbar/Logo.js
@@ -1,9 +1,16 @@
 import classes from './Logo.module.css'
 import logoImg from '../../../assets/logo/logo.png'
+import {NavLink} from "react-router-dom";
 
 const Logo = props => {
+    //
+    // <NavLink to={`${props.linkTo}`}
+    //          className={(({isActive}) => isActive ? (classes.link, classes.active) : (classes.link))}
+    //          end>{props.optionName}</NavLink>
     return (
+        <NavLink to={"/"}>
             <img className={classes.logo} src={logoImg} alt="Logo"/>
+        </NavLink>
     )
 }
 
