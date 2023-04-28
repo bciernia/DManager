3ff6f80149de0a77b47955273a1c4cf7cfa21b1c
Bartek <cierniabartek0802@gmail.com>
2023-04-28 21:59:03 +0200

    changed footer

--- client/src/components/Layout/Footer/Footer.js
+++ client/src/components/Layout/Footer/Footer.js
@@ -2,23 +2,27 @@ import classes from "./Footer.module.css";
 import Card from "../../UI/Card/Card";
 import Timer from "../Main/SessionStarter/Timer/Timer";
 import {useState} from "react";
+import {Grid} from "@mui/material";
+import Logo from "../Navbar/Logo";
+import Menu from "../Navbar/Menu/Menu";
+import MenuOption from "../Navbar/Menu/MenuOption";
+import {NavLink} from "react-router-dom";
 
 const Footer = () => {
-    const [isFooterVisible, setIsFooterVisible] = useState(false);
-
-    const timerBackground = isFooterVisible || classes.footer__hidden;
-    const changeFooterVisibilityBtnTitle = isFooterVisible ? 'Hide' : 'Show';
-
-    const changeVisibilityHandler = () => {
-        setIsFooterVisible(isFooterVisible => !isFooterVisible);
-    }
-
     return (
-        <div className={`${classes.container} ${timerBackground}`}>
-            <button className={classes["footer__btn--changeVisibility"]} onClick={changeVisibilityHandler}>{changeFooterVisibilityBtnTitle}</button>
-            <Card>
-                <Timer />
-            </Card>
+        <div className={classes.container}>
+            <Grid container spacing={2} sx={{alignItems: "center"}}>
+                <Grid item md={10}>
+
+                </Grid>
+                <Grid item md={2}>
+                    <NavLink to={'/contact'}
+                             className={(isActive) =>
+                                 isActive ? classes["contact-us__link"] : undefined}>
+                        Contact us!
+                    </NavLink>
+                </Grid>
+            </Grid>
         </div>
     )
 }
