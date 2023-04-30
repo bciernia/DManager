e4b3ce9e24a06b3057eb9758607bf85e970e9505
Bartek <cierniabartek0802@gmail.com>
2023-04-30 14:33:40 +0200

    added validation to sending mail

--- client/src/components/ContactUs/Form/ContactUsForm.js
+++ client/src/components/ContactUs/Form/ContactUsForm.js
@@ -3,19 +3,33 @@ import React, {useState} from "react";
 import classes from './ContactUsForm.module.css';
 import Card from "../../UI/Card/Card";
 import {useField} from "formik";
+import {useNavigate} from "react-router-dom";
 
 const ContactUsForm = props => {
+
+    const navigate = useNavigate();
 
     const [fullName, setFullName] = useState('');
     const [emailAddress, setEmailAddress] = useState('');
     const [emailSubject, setEmailSubject] = useState('');
     const [emailContent, setEmailContent] = useState('');
-    const [touched, setTouched] = useState(false);
+
+    const [nameTouched, setNameTouched] = useState(false);
+    const [mailTouched, setMailTouched] = useState(false);
+    const [subjectTouched, setSubjectTouched] = useState(false);
+    const [contentTouched, setContentTouched] = useState(false);
 
     const isNameValid = name => name.length < 2 || name.length > 50;
-    const isMailSubjectValid = mail => mail.length > 50;
+    const isMailValid = mail => mail.length < 2 || mail.length > 50;
+    const isMailSubjectValid = mail => mail.length < 2 || mail.length > 50;
+    const isMailContentValid = content => content.length < 2 || content.length > 50;
 
-    const handleSubmit = () => {
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        if (isNameValid(fullName) || isMailValid(emailAddress) || isMailSubjectValid(emailSubject) || isMailContentValid(emailContent)) {
+            return;
+        }
 
         const userMessage = {
             fullName,
@@ -24,31 +38,41 @@ const ContactUsForm = props => {
             emailContent,
         }
         props.sendMail(userMessage);
+
+        //TODO thank you for your feedback site
+        // navigate("");
     }
 
     return (
         <Card>
             <Typography variant="h2" textAlign="center" marginBottom="1rem">Contact us!</Typography>
-            <form className={classes.form} onSubmit={handleSubmit}>
+            <form className={classes.form} onSubmit={(event) => handleSubmit(event)}>
                 <TextField sx={{width: "80%"}} type="text" label="Full name"
-                           error={touched && isNameValid(fullName)}
-                           helperText={(touched && isNameValid(fullName)) ? "Name must have more than 2 characters and less than 20" : ""}
+                           error={nameTouched && isNameValid(fullName)}
+                           helperText={(nameTouched && isNameValid(fullName)) ? "Name must have more than 2 characters and less than 20" : ""}
                            inputProps={{maxLength: 30}}
-                           onBlur={() => setTouched(true)}
+                           onBlur={() => setNameTouched(true)}
                            required
                            onChange={(event) => setFullName(event.target.value)}/>
                 <TextField sx={{width: "80%"}} type="email" label="E-mail address"
                            inputProps={{maxLength: 50}}
+                           error={mailTouched && isMailValid(emailAddress)}
+                           helperText={(mailTouched && isMailValid(emailAddress)) ? "E-mail address is required" : ""}
+                           onBlur={() => setMailTouched(true)}
                            required
                            onChange={(event) => setEmailAddress(event.target.value)}/>
                 <TextField sx={{width: "80%"}} type="text" label="E-mail subject"
-                           error={isMailSubjectValid(emailSubject)}
-                           helperText={isMailSubjectValid(emailSubject) ? "E-mail subject can not be longer than 50 characters" : ""}
+                           error={subjectTouched && isMailSubjectValid(emailSubject)}
+                           helperText={(subjectTouched && isMailSubjectValid(emailSubject)) ? "E-mail subject is required and can not be longer than 50 characters" : ""}
+                           onBlur={() => setSubjectTouched(true)}
                            required
                            inputProps={{maxLength: 50}}
                            onChange={(event) => setEmailSubject(event.target.value)}/>
                 <TextField sx={{width: "80%"}} type="text" inputProps={{maxLength: 1000}} multiline minRows={5}
                            maxRows={5} label="E-mail content"
+                           error={contentTouched && isMailContentValid(emailContent)}
+                           helperText={(contentTouched && isMailContentValid(emailContent)) ? "E-mail content is required and can not be longer than 1000 characters " : ""}
+                           onBlur={() => setContentTouched(true)}
                            required
                            onChange={(event) => setEmailContent(event.target.value)}/>
                 <div className={classes["characters-counter"]}><p>{emailContent.length}/1000</p></div>
