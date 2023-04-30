e690677cf7be349dfc56a19ad0196db6def2c975
Bartek <cierniabartek0802@gmail.com>
2023-05-01 00:43:40 +0200

    Deleted unnecessary dependencies. Added links to create character forms by type

--- client/src/components/DMFacilities/Character/AllCharacters/AllCharacters.js
+++ client/src/components/DMFacilities/Character/AllCharacters/AllCharacters.js
@@ -1,16 +1,10 @@
-import Character from "../Character";
 import {useEffect, useState} from "react";
-import Spinner from "../../../UI/Spinner/Spinner";
 import {Link, useNavigate, useParams} from "react-router-dom";
 import Wrapper from "../../../../utils/Wrapper";
 import {Box, Button, Grid, List, ListItem, ListItemButton, ListItemText, Typography} from "@mui/material";
 import classes from './AllCharacters.module.css';
-import CampaignDetails from "../../Scenarios/CampaignDetails/CampaignDetails";
 import CharacterDetails from "../CharacterDetails/CharacterDetails";
-import CreateNewPlayerCharacter from "../CreateCharacter/CreateNewPlayerCharacter";
-import CreateNewNpc from "../CreateCharacter/CreateNewNpc";
-import CreateNewBeast from "../CreateCharacter/CreateNewBeast";
-import CreateNewMonster from "../CreateCharacter/CreateNewMonster";
+import Card from "../../../UI/Card/Card";
 
 const TeamDetails = props => {
     const params = useParams();
@@ -63,22 +57,33 @@ const TeamDetails = props => {
     return (
         <Wrapper>
             <div className={classes.container}>
-                <Button sx={{backgroundColor: "#F5793B"}} variant="contained" color="inherit" onClick={createPlayerCharacter}>Create player character</Button>
-                <Button sx={{backgroundColor: "#F5793B"}} variant="contained" color="inherit" onClick={createNpc}>Create npc</Button>
-                <Button sx={{backgroundColor: "#F5793B"}} variant="contained" color="inherit" onClick={createMonster}>Create monster</Button>
-                <Button sx={{backgroundColor: "#F5793B"}} variant="contained" color="inherit" onClick={createBeast}>Create beast</Button>
+                <Button sx={{backgroundColor: "#F5793B"}} variant="contained" color="inherit"
+                        onClick={createPlayerCharacter}>Create player character</Button>
+                <Button sx={{backgroundColor: "#F5793B"}} variant="contained" color="inherit" onClick={createNpc}>Create
+                    npc</Button>
+                <Button sx={{backgroundColor: "#F5793B"}} variant="contained" color="inherit" onClick={createMonster}>Create
+                    monster</Button>
+                <Button sx={{backgroundColor: "#F5793B"}} variant="contained" color="inherit" onClick={createBeast}>Create
+                    beast</Button>
             </div>
             <Grid container>
                 <Grid item md={2} sx={{display: "flex", flexDirection: "column", margin: "0.5rem"}}>
                     <Box sx={{width: "100%", margin: "0.5rem 0"}}>
                         <nav>
-                            <Typography variant="h4" textAlign="center">Campaign list</Typography>
-                            <List>
-                                {charactersArray.length === 0 && <Typography variant="h6" textAlign="center">No campaigns</Typography>}
+                            <Typography variant="h4" textAlign="center">Character list</Typography>
+                            <List sx={{
+                                height: "35rem",
+                                overflow: "auto",
+                                border: "solid 2px",
+                            }}>
+                                {charactersArray.length === 0 &&
+                                    <Typography variant="h6" textAlign="center">No characters</Typography>}
                                 {charactersArray.map(character =>
                                     <ListItem disablePadding>
-                                        <ListItemButton sx={{textAlign: "center"}} onClick={() => showCharacterDetails(character._id)}>
-                                            <ListItemText primary={character.characterName} secondary={character.characterType}
+                                        <ListItemButton sx={{textAlign: "center"}}
+                                                        onClick={() => showCharacterDetails(character._id)}>
+                                            <ListItemText primary={character.characterName}
+                                                          secondary={character.characterType}
                                             />
                                         </ListItemButton>
                                     </ListItem>
@@ -89,9 +94,11 @@ const TeamDetails = props => {
                 </Grid>
                 <Grid item md={9}>
                     <Box sx={{height: "50rem", width: "100%"}}>
-                        {!chosenCharacter ? (<Typography sx={{display:"flex", justifyContent:"center"}} variant="h2">Choose one of yours campaigns</Typography>) :
+                        {!chosenCharacter ? (
+                                <Typography sx={{display: "flex", justifyContent: "center"}} variant="h2">Choose one of
+                                    yours characters to display</Typography>) :
                             (
-                                <CharacterDetails character={chosenCharacter} />
+                                <CharacterDetails character={chosenCharacter}/>
                             )}
                     </Box>
                 </Grid>
