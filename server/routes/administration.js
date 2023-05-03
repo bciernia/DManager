aead2312f7e58813500e2a82ae0b493f05f049de
Bartek <cierniabartek0802@gmail.com>
2023-05-03 11:17:55 +0200

    Separated sending mails from administration route to system route

--- server/routes/administration.js
+++ server/routes/administration.js
@@ -1,29 +1,11 @@
 const express = require('express');
-const {transporter} = require("../utils/mail");
-const {MailCredentials} = require("../secrets/MailCredentials");
 
 const administrationRouter = express.Router();
 
 administrationRouter
 
-    .post('/', (req, res) => {
-        const mail = req.body;
-
-        const mailOptions = {
-            from: `${mail.fullName} <${mail.emailAddress}>`,
-            to: MailCredentials.login,
-            subject: mail.emailSubject,
-            text: mail.emailContent,
-        }
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if(error){
-                console.log(error);
-                res.send("error");
-            }else{
-                res.status(200).send(info);
-            }
-        })
+    .get('/', (req, res) => {
+        res.send("administration router");
     })
 
 module.exports = {
