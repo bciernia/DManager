27bbf566e986511674252519fedd6346898bba7f
Bartek <cierniabartek0802@gmail.com>
2023-05-03 15:12:35 +0200

    Added scenario routes

--- server/routes/dm.js
+++ server/routes/dm.js
@@ -2,6 +2,8 @@ const express = require('express');
 const {ObjectId} = require("mongodb");
 const {NpcRecord} = require("../records/npc.record");
 const {CampaignRecord} = require("../records/campaign.record");
+const {ScenarioRecord} = require("../records/scenario.record");
+const {CharacterRecord} = require("../records/character.record");
 
 const dmRouter = express.Router();
 
@@ -37,21 +39,6 @@ dmRouter
 
     })
 
-    //ADD NEW CAMPAIGN
-    .post('/campaign/newCampaign', async (req, res) => {
-        const campaign = req.body;
-
-        const newCampaign = new CampaignRecord({
-            campaignName: campaign.campaignName.toString(),
-            campaignSetting: campaign.campaignSetting.toString(),
-            campaignDescription: campaign.campaignDescription.toString(),
-        });
-
-        const newCampaignId = await newCampaign.insert();
-
-        res.status(201).send(newCampaignId);
-    })
-
     //GET ALL CAMPAIGNS
     .get('/campaign/all', async (req, res) => {
         const campaigns = await CampaignRecord.findAll();
@@ -66,6 +53,71 @@ dmRouter
         const campaign = await CampaignRecord.find(campaignId);
 
         res.status(200).send(campaign);
+    })
+
+    //ADD NEW CAMPAIGN
+    .post('/campaign/newCampaign', async (req, res) => {
+        const campaign = req.body;
+
+        const newCampaign = new CampaignRecord({
+            ...campaign,
+        });
+
+        const newCampaignId = await newCampaign.insert();
+
+        res.status(201).send(newCampaignId);
+    })
+
+    //DELETE CAMPAIGN
+    .delete('/campaign/:campaignId', async(req, res) => {
+        const {campaignId} = req.params;
+
+        const campaignToDelete = await CampaignRecord.find(campaignId);
+
+        await campaignToDelete.delete();
+
+        res.status(204).send();
+    })
+
+
+    //GET ALL SCENARIOS
+    .get('/scenario/all', async(req, res) => {
+        const scenarios = await ScenarioRecord.findAll();
+
+        res.status(200).send(scenarios);
+    })
+
+    //GET CHOSEN SCENARIO
+    .get('/scenario/:scenarioId', async(req, res) => {
+        const {scenarioId} = req.params;
+
+        const scenario = await ScenarioRecord.find(scenarioId);
+
+        res.status(200).send(scenario);
+    })
+
+    //ADD NEW SCENARIO
+    .post('/scenario/newScenario', async (req, res) => {
+        const scenario = req.body;
+
+        const newScenario = new CampaignRecord({
+            ...scenario
+        });
+
+        const newScenarioId = await newScenario.insert();
+
+        res.status(201).send(newScenarioId);
+    })
+
+    //DELETE SCENARIO
+    .delete('/scenario/:scenarioId', async(req, res) => {
+        const {scenarioId} = req.params;
+
+        const scenarioToDelete = await ScenarioRecord.find(scenarioId);
+
+        await scenarioToDelete.delete();
+
+        res.status(204).send();
     })
 
 module.exports = {
