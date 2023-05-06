0275b6dfdb1143787f91fac0ab7fc2a60e067b82
Bartek <cierniabartek0802@gmail.com>
2023-05-07 00:51:29 +0200

    Created scenario details partial view

--- client/src/components/DMFacilities/Campaign/CampaignDetails/ScenarioDetailsPartialView/ScenarioDetailsPartialView.js
+++ client/src/components/DMFacilities/Campaign/CampaignDetails/ScenarioDetailsPartialView/ScenarioDetailsPartialView.js
@@ -1,8 +1,27 @@
-const ScenarioDetailsPartialView = () => {
+import {Grid, Typography} from "@mui/material";
+
+const ScenarioDetailsPartialView = props => {
+    const scenario = props.scenario;
+
     return (
         <div>
-        test
-    </div>
+            <Typography variant="h2">{scenario.scenarioName}</Typography>
+            <Typography variant="h4">{scenario.scenarioDescription}</Typography>
+            <Grid container justifyContent="flex-start" alignItems="stretch" direction="column">
+                <Grid item sx={{backgroundColor: "whitesmoke", margin: ".5rem .5rem"}}>
+                    Notes
+                </Grid>
+                <Grid item sx={{backgroundColor: "whitesmoke", margin: ".5rem .5rem"}}>
+                    Characters?
+                </Grid>
+                <Grid item sx={{backgroundColor: "whitesmoke", margin: ".5rem .5rem"}}>
+                    Locations
+                </Grid>
+                <Grid item sx={{backgroundColor: "whitesmoke", margin: ".5rem .5rem"}}>
+                    Handouts
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
