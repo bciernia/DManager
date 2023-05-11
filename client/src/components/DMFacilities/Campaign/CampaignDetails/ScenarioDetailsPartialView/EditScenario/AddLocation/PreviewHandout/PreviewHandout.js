8de0c2b77a887db2ac958b2c4799a16be9866b3c
Bartek <cierniabartek0802@gmail.com>
2023-05-11 21:44:09 +0200

    Fixed bug with invisible handout description

--- client/src/components/DMFacilities/Campaign/CampaignDetails/ScenarioDetailsPartialView/EditScenario/AddLocation/PreviewHandout/PreviewHandout.js
+++ client/src/components/DMFacilities/Campaign/CampaignDetails/ScenarioDetailsPartialView/EditScenario/AddLocation/PreviewHandout/PreviewHandout.js
@@ -20,12 +20,12 @@ const PreviewHandout = props => {
     return (
         <>
             <Dialog onClose={handleClose} open={dialogOpen} maxWidth="md">
-                <Card sx={{height: "30rem", width: "20rem", padding: ".5rem"}}>
+                <Card sx={{minHeight: "25rem", height: "100%", width: "20rem", padding: ".5rem"}}>
                     {handout.handoutPhoto ? (<div className={classes["handout--photo__container"]}><img src={handout.handoutPhoto} className={classes["handout--photo"]}/></div>) :
                         <Typography variant="body1" sx={{textAlign: "center", margin: "1rem 0", fontStyle: "italic"}}>No image</Typography>}
                     <Divider/>
                     <Typography variant="body1" sx={{marginTop: ".25rem"}}>{handout.handoutName}</Typography>
-                    <Typography variant="body2" sx={{marginTop: ".5rem"}}> {handout.handoutDescription}</Typography>
+                    <Typography variant="body2" sx={{marginTop: ".5rem" }}> {handout.handoutDescription}</Typography>
                 </Card>
             </Dialog>
             <span onClick={previewHandout} className={classes["handout--display"]}>
