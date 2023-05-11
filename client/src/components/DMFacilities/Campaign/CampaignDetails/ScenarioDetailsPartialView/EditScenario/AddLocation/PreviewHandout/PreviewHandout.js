d996745ad0d17f702d1e025a0234966a523e2c6c
Bartek <cierniabartek0802@gmail.com>
2023-05-11 21:36:08 +0200

    Added and stylized handout photo

--- client/src/components/DMFacilities/Campaign/CampaignDetails/ScenarioDetailsPartialView/EditScenario/AddLocation/PreviewHandout/PreviewHandout.js
+++ client/src/components/DMFacilities/Campaign/CampaignDetails/ScenarioDetailsPartialView/EditScenario/AddLocation/PreviewHandout/PreviewHandout.js
@@ -1,4 +1,4 @@
-import {Card, Dialog, Typography} from "@mui/material";
+import {Card, Dialog, Divider, Typography} from "@mui/material";
 import React, {useState} from "react";
 import classes from "./PreviewHandout.module.css";
 
@@ -20,11 +20,12 @@ const PreviewHandout = props => {
     return (
         <>
             <Dialog onClose={handleClose} open={dialogOpen} maxWidth="md">
-                <Card sx={{height: "20rem", width: "12.5rem"}}>
-                    {handout.handoutPhoto}
-                    <Typography>{handout.handoutName}</Typography>
-                    <Typography> {handout.handoutDescription}</Typography>
-                    <Typography>{handout.handoutLocation}</Typography>
+                <Card sx={{height: "30rem", width: "20rem", padding: ".5rem"}}>
+                    {handout.handoutPhoto ? (<div className={classes["handout--photo__container"]}><img src={handout.handoutPhoto} className={classes["handout--photo"]}/></div>) :
+                        <Typography variant="body1" sx={{textAlign: "center", margin: "1rem 0", fontStyle: "italic"}}>No image</Typography>}
+                    <Divider/>
+                    <Typography variant="body1" sx={{marginTop: ".25rem"}}>{handout.handoutName}</Typography>
+                    <Typography variant="body2" sx={{marginTop: ".5rem"}}> {handout.handoutDescription}</Typography>
                 </Card>
             </Dialog>
             <span onClick={previewHandout} className={classes["handout--display"]}>
