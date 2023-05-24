bb6013a3b1bcf01ba28015d3db74e70bfed5440f
Barti <cierniabartek0802@gmail.com>
2023-05-24 22:07:27 +0200

    Added preview chosen character in add character to chosen scenario module

--- client/src/components/DMFacilities/Campaign/CampaignDetails/ScenarioDetailsPartialView/EditScenario/AddCharacterToScenario/PreviewChosenCharacter/PreviewChosenCharacter.js
+++ client/src/components/DMFacilities/Campaign/CampaignDetails/ScenarioDetailsPartialView/EditScenario/AddCharacterToScenario/PreviewChosenCharacter/PreviewChosenCharacter.js
@@ -1,11 +1,36 @@
+import classes from './PreviewChosenCharacter.module.css';
+import {Button, Card, Dialog, Divider, Typography} from "@mui/material";
+import React, {useState} from "react";
+
 const PreviewChosenCharacter = props => {
     const character = props.character;
 
+    const [characterDetailsDialogOpen, setCharacterDetailsDialogOpen] = useState(false);
+
+    const handleCharacterDetailsDialogClose = () => {
+        setCharacterDetailsDialogOpen(false);
+    };
+
+    const handleCharacterDetailsDialogOpen = () => {
+        setCharacterDetailsDialogOpen(true);
+    };
+
     return (
-        <div>
-            Preview chosen character
-            {character.characterName}
-        </div>
+        <>
+            <Dialog onClose={handleCharacterDetailsDialogClose} open={characterDetailsDialogOpen} maxWidth="md">
+
+            </Dialog>
+            <Card sx={{padding: ".5rem .5rem"}}>
+                <div className={classes["img-container"]}>
+                    <img src={character.characterPhoto} alt="Character photo" className={classes["character-photo"]}/>
+                </div>
+                <Divider/>
+                <Typography>{character.characterName}</Typography>
+
+                <Button>Add</Button>
+                <Button onClick={handleCharacterDetailsDialogOpen}>Details</Button>
+            </Card>
+        </>
     )
 }
 
