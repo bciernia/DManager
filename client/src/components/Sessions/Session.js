b031d42e8c83b716d7eb176a79093a3450804588
Barti <cierniabartek0802@gmail.com>
2023-06-09 10:58:42 +0200

    Displayed notes in session page

--- client/src/components/Sessions/Session.js
+++ client/src/components/Sessions/Session.js
@@ -41,9 +41,38 @@ const Session = props => {
 
     return (
         <div>
-            CURRENT SESSION
+            <div><Typography variant="h5">{scenario.scenarioName}</Typography></div>
+            <div>
+                <Typography variant="h6">Scenario handouts</Typography>
+                {handouts.map(handout => <PreviewHandout handout={handout}/>)}
+            </div>
             <Grid container>
-                <Grid item md={4}>
+                <Grid item md={2.5}>
+                    <Typography variant="h5"
+                                sx={{marginBottom: ".5rem"}}>Notes</Typography>
+                    <List sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        height: "35rem",
+                        overflow: "auto",
+                        overflowX: "hidden",
+                    }}>
+                        {/*TODO save note to db after adding them*/}
+                        {notes?.length === 0 &&
+                            <Typography variant="h6" textAlign="center">No notes</Typography>}
+                        {notes.map((note) =>
+                            <ListItem key={note._id}
+                                      sx={{margin: ".25rem", display: "flex", justifyContent: "center"}}
+                                      disablePadding>
+                                <Card sx={{backgroundColor: "whitesmoke", width: 320}}>
+                                        <ListItemText sx={{padding: ".25rem"}}
+                                            primary={<Typography variant="body2">{note.note}</Typography>}/>
+                                </Card>
+                            </ListItem>
+                        )}
+                    </List>
+                </Grid>
+                <Grid item md={3.5}>
                     <PreviewLocation scenarioId={scenarioId} isInEditingScenario={false}/>
                 </Grid>
             </Grid>
