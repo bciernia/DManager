a1075d5dab7d093e55f9736664c104ca01774f2a
Barti <cierniabartek0802@gmail.com>
2023-06-10 21:17:37 +0200

    Changed layout in session page

--- client/src/components/Sessions/Session.js
+++ client/src/components/Sessions/Session.js
@@ -1,9 +1,11 @@
 import PreviewLocation from "../DMFacilities/Location/PreviewLocation/PreviewLocation";
 import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
-import {Card, Grid, List, ListItem, ListItemButton, ListItemText, Typography} from "@mui/material";
+import {Box, Card, Grid, List, ListItem, ListItemText, Tab, Tabs, Typography} from "@mui/material";
 import PreviewHandout
     from "../DMFacilities/Campaign/CampaignDetails/ScenarioDetailsPartialView/EditScenario/AddHandout/PreviewHandout/PreviewHandout";
+import Timer from "../Layout/Main/SessionStarter/Timer/Timer";
+import {TabContext, TabPanel} from "@mui/lab";
 
 const getScenarioById = (scenarioId) =>
     fetch(`http://127.0.0.1:3000/dm/scenario/${scenarioId}`)
@@ -26,6 +28,7 @@ const Session = props => {
     const [handouts, setHandouts] = useState([]);
     const [notes, setNotes] = useState([]);
 
+    const [value, setValue] = React.useState('one');
 
     useEffect(() => {
         Promise.all([
@@ -39,43 +42,104 @@ const Session = props => {
         })
     }, []);
 
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
         <div>
-            <div><Typography variant="h5">{scenario.scenarioName}</Typography></div>
-            <div>
-                <Typography variant="h6">Scenario handouts</Typography>
-                {handouts.map(handout => <PreviewHandout handout={handout}/>)}
-            </div>
-            <Grid container>
-                <Grid item md={2.5}>
-                    <Typography variant="h5"
-                                sx={{marginBottom: ".5rem"}}>Notes</Typography>
-                    <List sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        height: "35rem",
-                        overflow: "auto",
-                        overflowX: "hidden",
-                    }}>
-                        {/*TODO save note to db after adding them*/}
-                        {notes?.length === 0 &&
-                            <Typography variant="h6" textAlign="center">No notes</Typography>}
-                        {notes.map((note) =>
-                            <ListItem key={note._id}
-                                      sx={{margin: ".25rem", display: "flex", justifyContent: "center"}}
-                                      disablePadding>
-                                <Card sx={{backgroundColor: "whitesmoke", width: 320}}>
+            <TabContext value={value}>
+                <Grid container>
+                    <Grid item md={3}>
+                        <Grid container>
+                            <Grid item md={12}>
+                                <Typography variant="h6" sx={{marginLeft:".5rem"}}>Scenario name</Typography>
+                            </Grid>
+                            <Grid item md={12}>
+                                <Typography sx variant="h5" sx={{margin: ".5rem"}}>{scenario.scenarioName}</Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item md={6}>
+                        <Tabs
+                            value={value}
+                            onChange={handleChange}
+                            textColor="inherit"
+                            aria-label="secondary tabs example"
+                            variant="fullWidth"
+                            sx={{
+                                ".Mui-selected": {
+                                    color: "#F5793B",
+                                },
+                            }}
+                            TabIndicatorProps={{
+
+                                style: {
+                                    backgroundColor: "#F5793B",
+                                }
+                            }}
+                        >
+                            <Tab value="one" label="Players"/>
+                            <Tab value="two" label="Characters"/>
+                            <Tab value="three" label="Locations"/>
+                        </Tabs>
+                    </Grid>
+                    <Grid item md={3}>
+                        <Box
+                            sx={{display: "flex", justifyContent: "flex-end", margin: ".5rem .5rem 0 0"}}><Timer/></Box>
+                    </Grid>
+                </Grid>
+
+                <Grid container>
+                    <Grid item md={2}>
+                        <Typography variant="h6" sx={{marginLeft:".5rem"}}>Scenario description</Typography>
+                        <Typography variant="body2" sx={{margin:"1rem"}}>{scenario.scenarioDescription}</Typography>
+                        <Typography variant="h6" sx={{marginLeft:".5rem"}}>Scenario handouts</Typography>
+                        <Box sx={{marginLeft:".5rem"}}>{handouts.map(handout =>
+                            <PreviewHandout
+                                handout={handout}/>)}</Box>
+                    </Grid>
+
+                    <Grid item md={7}>
+                        <TabPanel value="one" index={0}>
+                            <Grid container>
+
+                            </Grid>
+                        </TabPanel>
+                        <TabPanel value="two" index={1}>
+                            Item Two
+                        </TabPanel>
+                        <TabPanel value="three" index={2}>
+                            <PreviewLocation scenarioId={scenarioId} isInEditingScenario={false}/>
+                        </TabPanel>
+                    </Grid>
+
+                    <Grid item md={3}>
+                        <List sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            height: "35rem",
+                            overflow: "auto",
+                            overflowX: "hidden",
+                        }}>
+                            {/*TODO save note to db after adding them*/}
+                            {notes?.length === 0 &&
+                                <Typography variant="h6" textAlign="center">No notes</Typography>}
+                            {notes.map((note) =>
+                                <ListItem key={note._id}
+                                          sx={{margin: ".25rem", display: "flex", justifyContent: "center"}}
+                                          disablePadding>
+                                    <Card sx={{backgroundColor: "whitesmoke", width: 320}}>
                                         <ListItemText sx={{padding: ".25rem"}}
-                                            primary={<Typography variant="body2">{note.note}</Typography>}/>
-                                </Card>
-                            </ListItem>
-                        )}
-                    </List>
+                                                      primary={<Typography variant="body2">{note.note}</Typography>}/>
+                                    </Card>
+                                </ListItem>
+                            )}
+                        </List>
+                    </Grid>
                 </Grid>
-                <Grid item md={3.5}>
-                    <PreviewLocation scenarioId={scenarioId} isInEditingScenario={false}/>
-                </Grid>
-            </Grid>
+
+            </TabContext>
         </div>
     )
 }
